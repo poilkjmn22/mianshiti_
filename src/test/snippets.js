30c524b2e1c12155cfb781424c30025f59ab1380
qi.fang <qi.fang@wenba100.com>
2020-04-01 17:27:23 +0800

    add: src/test/**.*

---
+++ src/test/snippets.js
@@ -0,0 +1,31 @@
+console.dir();
+
+for (var i = 0; i < array.length; i++) {
+  array[i]
+}
+
+while (true) {
+
+}
+
+methodName: function (attribute) {
+
+},
+
+key: 'value',
+
+condition ? true : false
+
+setTimeout(function () {
+
+}, 10);
+
+() => {
+
+}
+
+(param) => {
+
+}
+
+import { interface } from 'module';
